bd99602159322f4a09d5d22bec9b39900e23dfc4
mads jerup <madsjerup@gmail.com>
2023-10-17 22:24:44 +0200

    w-80 to w-text

--- src/routes/FAQ/+page.svelte
+++ src/routes/FAQ/+page.svelte
@@ -6,7 +6,7 @@
 <div class="w-[100%] h-[100%] flex flex-row justify-center ">
     <div class="flex flex-col">
         <h1 class="text-5xl text-center">FAQ</h1>
-        <div class="w-80 m-5">
+        <div class="w-text m-5">
             {#each data.data as FA }
             <details>
                 <summary class="font-bold">
