071898df9c9de2183c5ba31364a9c7e4034913bf
mads jerup <madsjerup@gmail.com>
2023-10-17 23:05:28 +0200

    Design forbedringer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,22 @@
 <div>
-
     <div class="w-[100%] h-[100%] flex flex-row justify-center">
         <div class="w-text m-5">
-            <h1 class="text-5xl">Om Fagrådet</h1>
+            <h1 class="text-5xl text-center p-2">Om Fagrådet</h1>
             <p class="pt-2">
-                Fagrådet ved Datalogisk Institut på Københavns Universitet (DIKU) er nyopstartet og har til formål at styrke studiemiljøet, repræsentere studerendes interesser, og fremme samarbejdet mellem studerende og Institut. Dette inkluderer studievejledning, arrangementer, feedbackindsamling, ressourcefordeling, og bidrag til studieudvikling for at skabe en positiv studieoplevelse.
+                Fagrådet ved Datalogisk Institut på Københavns Universitet
+                (DIKU) er nyopstartet og har til formål at styrke studiemiljøet,
+                repræsentere studerendes interesser, og fremme samarbejdet
+                mellem studerende og Institut. Dette inkluderer
+                studievejledning, arrangementer, feedbackindsamling,
+                ressourcefordeling, og bidrag til studieudvikling for at skabe
+                en positiv studieoplevelse.
             </p>
-            <h2 class="text-3xl pt-5">Bestyrelse</h2>
-            <p class="pt-2"> bliver tilføjet når bestyrelsen er valgt</p>
-            <h2 class="text-3xl pt-5">Vedtægter</h2>
+            <h2 class="text-3xl text-center p-2">Bestyrelse</h2>
+            <p class="pt-2">bliver tilføjet når bestyrelsen er valgt</p>
+            <h2 class="text-3xl text-center p-2">Vedtægter</h2>
             <p class="pt-2">Bliver tilføjet når vedtægter er godkendt</p>
-            <h2 class="text-3xl pt-5">Refererer</h2>
+            <h2 class="text-3xl text-center p-2">Refererer</h2>
             <p class="pt-2">Bliver tilføjet efter næste møde</p>
         </div>
-    
     </div>
-
 </div>
