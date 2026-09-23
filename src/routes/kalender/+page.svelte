89035d41f7b5f72c25b5167d54427c6ed6ba0cc4
David JT <djt@hotmail.dk>
2026-09-23 14:53:09 +0200

    Update text for adding DI'Kalender to own calendar

--- src/routes/kalender/+page.svelte
+++ src/routes/kalender/+page.svelte
@@ -7,7 +7,7 @@
                DIKU Fagråds DI'Kalender er en samlet kalender over alle de studerendes arrangementer, så du som DIKU-studerende nemmere kan deltage i alle de fantastiske arrangementer. Du kan læse mere og tilmelde dig hvert arrangement gennem linket i begivenhedernes beskrivelser.
             </p>
             <p class="pt-2">
-                Det anbefales at tilføje DI'Kalender til ens egen kalender, så man altid har overblik over alle arrangementerne. Kalenderen kan tilføjes til Google Calendar med knappen nederst i kalenderen herunder eller ved at kopiere <a class="text-blue-700" target="_blank" href="https://calendar.google.com/calendar/ical/d4fffacfd512816188d29cfdb9918bcbffcc5bf80195f2cd97f5075207702f87%40group.calendar.google.com/public/basic.ics">dette iCal-link</a> ind i ens egen kalender.
+                Det anbefales at tilføje DI'Kalender til ens egen kalender, så du altid har overblik over alle arrangementerne. Kalenderen kan tilføjes til Google Calendar med knappen nederst i kalenderen herunder eller ved at kopiere <a class="text-blue-700" target="_blank" href="https://calendar.google.com/calendar/ical/d4fffacfd512816188d29cfdb9918bcbffcc5bf80195f2cd97f5075207702f87%40group.calendar.google.com/public/basic.ics">dette iCal-link</a> ind i din egen kalender.
             </p>
             <div>
                 <h2 class="text-3xl pt-5">DI'Kalender</h2>
